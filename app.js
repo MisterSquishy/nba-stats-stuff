5ef605a07c005bf2fe169d23338c6db5861e8f13
MisterSquishy <pdavids@comcast.net>
2017-11-29 21:38:38 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var chart = require('chart');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
