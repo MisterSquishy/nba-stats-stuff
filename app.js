9b9964e285e7a6f68aaa10e6c227ce46b71e92b2
MisterSquishy <pdavids@comcast.net>
2017-12-05 21:18:30 -0500

    call out to correct stats API

--- app.js
+++ app.js
@@ -17,7 +17,30 @@ app.get('/', function (req, res) {
   emitter.on('calloutserror', function(err) {
     res.send(err);
   });
-  performRequest('/stats/commonallplayers', 'GET', {'LeagueID':'00','Season':'2017-18','IsOnlyCurrentSeason':'1'});
+  performRequest('/stats/leaguedashplayerstats', 'GET', {'SeasonType':'Regular Season',
+                                                          'MeasureType':'Base',
+                                                          'PerMode':'Totals',
+                                                          'PlusMinus':'Y',
+                                                          'PaceAdjust':'N',
+                                                          'Rank':'N',
+                                                          'Season':'2017-18',
+                                                          'Month':'0',
+                                                          'LastNGames':'0',
+                                                          'Period':'0',
+                                                          'OpponentTeamID':'0',
+                                                          'DateFrom':'',
+                                                          'DateTo':'',
+                                                          'VsConference':'',
+                                                          'GameSegment':'',
+                                                          'SeasonSegment':'',
+                                                          'GameScope':'',
+                                                          'PlayerExperience':'',
+                                                          'PlayerPosition':'',
+                                                          'StarterBench':'',
+                                                          'VsDivision':'',
+                                                          'Outcome':'',
+                                                          'Location':''});
+  // performRequest('/stats/commonallplayers', 'GET', {'LeagueID':'00','Season':'2017-18','IsOnlyCurrentSeason':'1'});
 
 });
 
