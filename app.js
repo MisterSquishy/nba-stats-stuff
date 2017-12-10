043cdc2fc58635709ff0383573afe32703ec2628
MisterSquishy <pdavids@comcast.net>
2017-12-10 16:30:51 -0500

    datepicker is working; today's stats arent

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var app = express();
 var pug = require('pug');
 var callouts = require('./Utils/callouts');
 var NBAAPIConstants = require('./Utils/NBAAPIConstants');
@@ -6,38 +7,73 @@ var FantasyConstants = require('./Utils/FantasyConstants');
 var dataReformatter = require('./Utils/dataReformatter');
 const compiledFunction = pug.compileFile('./templates/home.pug');
 
+var appRes;
+var dateOption;
 const emitter = require('./Utils/globalEmitter');
-var app = express();
+emitter.on('calloutsuccess', function(result) {
+  var formattedData = dataReformatter.reformatNBAPlayerDashboard('LEAGUE_DASHBOARD_API', result.headers, result.rowSet);
 
-app.get('/', function (req, res) {
-  emitter.on('calloutsuccess', function(result) {
-    var formattedData = dataReformatter.reformatNBAPlayerDashboard('LEAGUE_DASHBOARD_API', result.headers, result.rowSet);
+  appRes.end(compiledFunction({
+    headers:formattedData[0],
+    rowSet:formattedData[1],
+    teamRosters:JSON.stringify(FantasyConstants),
+    fantasyScoring:JSON.stringify(FantasyConstants.scoring()),
+    dateOption:dateOption
+  }));
+});
+emitter.on('calloutserror', function(err) {
+  appRes.send(err);
+});
 
-    res.end(compiledFunction({
-      headers:formattedData[0],
-      rowSet:formattedData[1],
-      teamRosters:JSON.stringify(FantasyConstants),
-      fantasyScoring:JSON.stringify(FantasyConstants.scoring())
-    }));
-  });
-  emitter.on('calloutserror', function(err) {
-    res.send(err);
-  });
+var processRequest = function() {
+  var params = JSON.parse(JSON.stringify(NBAAPIConstants.LEAGUE_DASHBOARD_API.Params)); //really???
+  if (dateOption !== 'Full') {
+    var date = new Date();
+    if (dateOption === 'Today') {
+      //date set by default
+    }
+    else if (dateOption === 'Yesterday') {
+      date.setDate(date.getDate() - 1);
+    }
 
-  var params = NBAAPIConstants.LEAGUE_DASHBOARD_API.Params;
-  var date = new Date();
-  //if no params, figure out if today or yesterday (fallback behavior)
-  if (date.getHours() < 12) {
-    //show yesterday before noon
-    date.setDate(date.getDate() - 1);
+    params.DateFrom = date.toISOString();
+    params.DateTo = date.toISOString();
   }
-  params.DateFrom = date.toISOString();
-  params.DateTo = date.toISOString();
   callouts.performRequest(NBAAPIConstants.LEAGUE_DASHBOARD_API.URI, 'GET', params);
   // callouts.performRequest('/players/'+LastName+'/'+FirstName, 'GET', null, true); //todo images??
+};
 
+app.get('/', function (req, res) {
+  appRes = res;
+  //if no params, figure out if today or yesterday (fallback behavior)
+  var date = new Date();
+  if (date.getHours() < 12) {
+    dateOption = 'Yesterday';
+  }
+  else {
+    dateOption = 'Today';
+  }
+  processRequest();
+});
+
+app.get('/Today', function (req, res) {
+  appRes = res;
+  dateOption = 'Today';
+  processRequest();
+});
+
+app.get('/Yesterday', function (req, res) {
+  appRes = res;
+  dateOption = 'Yesterday';
+  processRequest();
+});
+
+app.get('/Full', function (req, res) {
+  appRes = res;
+  dateOption = 'Full';
+  processRequest();
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('App listening on port 3000!');
 });
