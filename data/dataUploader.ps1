9b030340df88e0fd37ee990cf4cba6bbf1d7be2f
PJDDataUploader <pdavids02@yahoo.com>
2017-12-16 13:55:06 -0500

    add data update script

--- data/dataUploader.ps1
+++ data/dataUploader.ps1
@@ -1,18 +1,46 @@
-﻿# get data from nba
-# todo update dates in environment file
-$date = Get-Date -UFormat "%Y-%m-%d"
-newman run NBAStats.postman_collection.json -e date.post_environment.json -r 'json' --reporter-json-export CombinedData.json
+﻿function Parse-GzipStreamData {
+    Param ([Byte[]]$b)
+
+    $sb = New-Object -TypeName "System.Text.StringBuilder";
+
+    $gZipStream = New-Object System.IO.MemoryStream( , $b )
+    $gZipData = New-Object IO.Compression.GZipStream $gZipStream, ([System.IO.Compression.CompressionMode]::Decompress)
+    $gZipBaseStream = $gZipData.BaseStream
+    $parsedgZipString = ""
+    for($i = 0; $i -lt $gZipBaseStream.Length; $i++) {
+        $parsedgZipString = $sb.Append($([System.Text.Encoding]::ASCII.GetString($gZipBaseStream.ReadByte())))
+    }
+    return $parsedgZipString
+}
+
+Set-Location $(Split-Path $MyInvocation.MyCommand.Path)
+
+# get data from nba
+$dateEnvironmentText = Get-Content -Raw -Path date.postman_environment.json
+$dateEnvironment = ConvertFrom-Json –InputObject $dateEnvironmentText
+$dateEnvironment.values[0].value = Get-Date -UFormat "%Y-%m-%d" #param 1 is today
+$dateEnvironment.values[1].value = Get-Date (Get-Date).AddDays(-1) -UFormat "%Y-%m-%d" #param 2 is yesterday
+$dateEnvironment | ConvertTo-Json -depth 10 | Set-Content "date.postman_environment.json"
+cmd /c "newman run NBAStats.postman_collection.json -e date.postman_environment.json -r json --reporter-json-export CombinedData.json"
 
 # update json files
-# todo parse stuff out of CombinedData.json
-$fulldata = ???
-$todaydata = ???
-$yesterdaydata = ???
+$CombinedDataText = Get-Content  -Raw -Path  CombinedData.json
+$CombinedData = ConvertFrom-Json –InputObject $CombinedDataText
+
+$fulldata = Parse-GzipStreamData $CombinedData.run.executions[0].response.stream.data #callout 1 is full
+$fulldata | Set-Content "full.json"
+$todaydata = Parse-GzipStreamData $CombinedData.run.executions[1].response.stream.data #callout 2 is today
+$todaydata | Set-Content "today.json"
+$yesterdaydata = Parse-GzipStreamData $CombinedData.run.executions[2].response.stream.data #callout 3 is yesterday
+$yesterdaydata | Set-Content "yesterday.json"
 
 # push up to heroku
-git add today.json yesterday.json full.json
-git commit -m "Data update " + $date
-heroku login
-echo "pdavids02@yahoo.com`r" | heroku
-echo "DataUploader1!`r" | heroku
-git push heroku master
+#GitHub: pdavids02@yahoo.com/TestPassword1!
+cmd.exe /c "git config user.email pdavids02@yahoo.com"
+cmd.exe /c "git config user.name PJDDataUploader"
+cmd.exe /c "git add today.json yesterday.json full.json date.postman_environment.json"
+$date = Get-Date
+cmd.exe /c "git commit -m `"Data update $date`""
+#Heroku: pdavids02@yahoo.com/DataUploader1!
+cmd.exe /c "git push heroku master"
+cmd.exe /c "git push origin master"
