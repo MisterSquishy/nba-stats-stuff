97381043c65144da97d7564b6e22cf94f021e385
PJDDataUploader <pdavids02@yahoo.com>
2017-12-26 09:56:43 -0500

    cache dropdown option choices

--- templates/scripts/dataTable.js
+++ templates/scripts/dataTable.js
@@ -103,3 +103,20 @@ footerCallback = function (row, data, start, end, display) {
     }
   }
 };
+setCookie = function(cookieName, cookieValue) { //this sucks
+  var today = new Date();
+  var expire = new Date();
+
+  expire.setTime(today.getTime() + 3600000*24);
+  document.cookie = cookieName+"="+escape(cookieValue) + ";expires="+expire.toGMTString();
+};
+readCookie = function(name, fallback) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return fallback;
+};
