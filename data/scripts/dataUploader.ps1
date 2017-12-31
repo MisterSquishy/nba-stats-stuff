e5230cfa9b13c78c68fda1f1f4ff0f8e2a32a4f7
PJDDataUploader <pdavids02@yahoo.com>
2017-12-31 12:01:13 -0500

    Data update 12/31/2017 12:01:13

--- data/scripts/dataUploader.ps1
+++ data/scripts/dataUploader.ps1
@@ -16,11 +16,20 @@
 Set-Location $(Split-Path $MyInvocation.MyCommand.Path)
 
 # get data from nba
+$daysFromSunday = @{"Sunday" = 7; "Monday" = 1; "Tuesday" = 2; "Wednesday" = 3; "Thursday" = 4; "Friday" = 5; "Saturday" = 6;}
 $dateEnvironmentText = Get-Content -Raw -Path date.postman_environment.json
 $dateEnvironment = ConvertFrom-Json –InputObject $dateEnvironmentText
 $dateEnvironment.values[0].value = Get-Date -UFormat "%Y-%m-%d" #param 1 is today
 $dateEnvironment.values[1].value = Get-Date (Get-Date).AddDays(-1) -UFormat "%Y-%m-%d" #param 2 is yesterday
-#TODO: get days of the week
+$lastSunday = (Get-Date).AddDays(-$daysFromSunday.Get_Item((Get-Date).DayOfWeek))
+$dateEnvironment.values[2].value = Get-Date $lastSunday -UFormat "%Y-%m-%d"
+$dateEnvironment.values[3].value = Get-Date $lastSunday.AddDays(1) -UFormat "%Y-%m-%d"
+$dateEnvironment.values[4].value = Get-Date $lastSunday.AddDays(2) -UFormat "%Y-%m-%d"
+$dateEnvironment.values[5].value = Get-Date $lastSunday.AddDays(3) -UFormat "%Y-%m-%d"
+$dateEnvironment.values[6].value = Get-Date $lastSunday.AddDays(4) -UFormat "%Y-%m-%d"
+$dateEnvironment.values[7].value = Get-Date $lastSunday.AddDays(5) -UFormat "%Y-%m-%d"
+$dateEnvironment.values[8].value = Get-Date $lastSunday.AddDays(6) -UFormat "%Y-%m-%d"
+
 $dateEnvironment | ConvertTo-Json -depth 10 | Set-Content "date.postman_environment.json"
 cmd /c "newman run NBAStats.postman_collection.json -e date.postman_environment.json -r json --reporter-json-export CombinedData.json"
 
@@ -50,16 +59,30 @@ $data = Parse-GzipStreamData $CombinedData.run.executions[9].response.stream.dat
 $data | Set-Content "../saturday.json"
 $data = Parse-GzipStreamData $CombinedData.run.executions[10].response.stream.data #callout 11 is full per game
 $data | Set-Content "../fullpergame.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[11].response.stream.data #callout 12 is scoreboard
+$data = Parse-GzipStreamData $CombinedData.run.executions[11].response.stream.data #today's scoreboard
 $data | Set-Content "../scoreboard.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[12].response.stream.data #callout 13 is allplayers
+$data = Parse-GzipStreamData $CombinedData.run.executions[12].response.stream.data #this week's scoreboard
+$data | Set-Content "../scoreboardSunday.json"
+$data = Parse-GzipStreamData $CombinedData.run.executions[13].response.stream.data
+$data | Set-Content "../scoreboardMonday.json"
+$data = Parse-GzipStreamData $CombinedData.run.executions[14].response.stream.data
+$data | Set-Content "../scoreboardTuesday.json"
+$data = Parse-GzipStreamData $CombinedData.run.executions[15].response.stream.data
+$data | Set-Content "../scoreboardWednesday.json"
+$data = Parse-GzipStreamData $CombinedData.run.executions[16].response.stream.data
+$data | Set-Content "../scoreboardThursday.json"
+$data = Parse-GzipStreamData $CombinedData.run.executions[17].response.stream.data
+$data | Set-Content "../scoreboardFriday.json"
+$data = Parse-GzipStreamData $CombinedData.run.executions[18].response.stream.data
+$data | Set-Content "../scoreboardSaturday.json"
+$data = Parse-GzipStreamData $CombinedData.run.executions[19].response.stream.data #callout 20 is allplayers
 $data | Set-Content "../allplayers.json"
 
 # push up to heroku
 #GitHub: pdavids02@yahoo.com/TestPassword1!
 cmd.exe /c "git config user.email pdavids02@yahoo.com"
 cmd.exe /c "git config user.name PJDDataUploader"
-cmd.exe /c "git add ../today.json ../yesterday.json ../full.json ../sunday.json ../monday.json ../tuesday.json ../wednesday.json ../thursday.json ../friday.json ../saturday.json ../fullpergame.json ../allplayers.json ../scoreboard.json date.postman_environment.json"
+cmd.exe /c "git add ../today.json ../yesterday.json ../full.json ../sunday.json ../monday.json ../tuesday.json ../wednesday.json ../thursday.json ../friday.json ../saturday.json ../fullpergame.json ../allplayers.json ../scoreboard.json ../scoreboardSunday.json ../scoreboardMonday.json ../scoreboardTuesday.json ../scoreboardWednesday.json ../scoreboardThursday.json ../scoreboardFriday.json ../scoreboardSaturday.json date.postman_environment.json"
 $date = Get-Date
 cmd.exe /c "git commit -m `"Data update $date`""
 #Heroku: pdavids02@yahoo.com/DataUploader1!
