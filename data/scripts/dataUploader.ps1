b48267ad521f605fbe5d0bc318f53595e7a1ace8
PJDDataUploader <pdavids02@yahoo.com>
2018-02-12 18:29:16 -0500

    weeks are now monday-sunday

--- data/scripts/dataUploader.ps1
+++ data/scripts/dataUploader.ps1
@@ -16,19 +16,19 @@
 Set-Location $(Split-Path $MyInvocation.MyCommand.Path)
 
 # get data from nba
-$daysFromSunday = @{"Sunday" = 7; "Monday" = 1; "Tuesday" = 2; "Wednesday" = 3; "Thursday" = 4; "Friday" = 5; "Saturday" = 6;}
+$daysFromMonday = @{"Sunday" = 6; "Monday" = 7; "Tuesday" = 1; "Wednesday" = 2; "Thursday" = 3; "Friday" = 4; "Saturday" = 5;}
 $dateEnvironmentText = Get-Content -Raw -Path date.postman_environment.json
 $dateEnvironment = ConvertFrom-Json –InputObject $dateEnvironmentText
 $dateEnvironment.values[0].value = Get-Date -UFormat "%Y-%m-%d" #param 1 is today
 $dateEnvironment.values[1].value = Get-Date (Get-Date).AddDays(-1) -UFormat "%Y-%m-%d" #param 2 is yesterday
-$lastSunday = (Get-Date).AddDays(-$daysFromSunday.Get_Item([string](Get-Date).DayOfWeek))
-$dateEnvironment.values[2].value = Get-Date $lastSunday -UFormat "%Y-%m-%d"
-$dateEnvironment.values[3].value = Get-Date $lastSunday.AddDays(1) -UFormat "%Y-%m-%d"
-$dateEnvironment.values[4].value = Get-Date $lastSunday.AddDays(2) -UFormat "%Y-%m-%d"
-$dateEnvironment.values[5].value = Get-Date $lastSunday.AddDays(3) -UFormat "%Y-%m-%d"
-$dateEnvironment.values[6].value = Get-Date $lastSunday.AddDays(4) -UFormat "%Y-%m-%d"
-$dateEnvironment.values[7].value = Get-Date $lastSunday.AddDays(5) -UFormat "%Y-%m-%d"
-$dateEnvironment.values[8].value = Get-Date $lastSunday.AddDays(6) -UFormat "%Y-%m-%d"
+$lastMonday = (Get-Date).AddDays(-$daysFromMonday.Get_Item([string](Get-Date).DayOfWeek))
+$dateEnvironment.values[2].value = Get-Date $lastMonday -UFormat "%Y-%m-%d"
+$dateEnvironment.values[3].value = Get-Date $lastMonday.AddDays(1) -UFormat "%Y-%m-%d"
+$dateEnvironment.values[4].value = Get-Date $lastMonday.AddDays(2) -UFormat "%Y-%m-%d"
+$dateEnvironment.values[5].value = Get-Date $lastMonday.AddDays(3) -UFormat "%Y-%m-%d"
+$dateEnvironment.values[6].value = Get-Date $lastMonday.AddDays(4) -UFormat "%Y-%m-%d"
+$dateEnvironment.values[7].value = Get-Date $lastMonday.AddDays(5) -UFormat "%Y-%m-%d"
+$dateEnvironment.values[8].value = Get-Date $lastMonday.AddDays(6) -UFormat "%Y-%m-%d"
 
 $dateEnvironment | ConvertTo-Json -depth 10 | Set-Content "date.postman_environment.json"
 newman run NBAStats.postman_collection.json -e date.postman_environment.json -r json --reporter-json-export CombinedData.json
@@ -44,37 +44,37 @@ $data | Set-Content "../today.json"
 $data = Parse-GzipStreamData $CombinedData.run.executions[2].response.stream.data #callout 3 is yesterday
 $data | Set-Content "../yesterday.json"
 $data = Parse-GzipStreamData $CombinedData.run.executions[3].response.stream.data #these are for days of the week
-$data | Set-Content "../sunday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[4].response.stream.data
 $data | Set-Content "../monday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[5].response.stream.data
+$data = Parse-GzipStreamData $CombinedData.run.executions[4].response.stream.data
 $data | Set-Content "../tuesday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[6].response.stream.data
+$data = Parse-GzipStreamData $CombinedData.run.executions[5].response.stream.data
 $data | Set-Content "../wednesday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[7].response.stream.data
+$data = Parse-GzipStreamData $CombinedData.run.executions[6].response.stream.data
 $data | Set-Content "../thursday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[8].response.stream.data
+$data = Parse-GzipStreamData $CombinedData.run.executions[7].response.stream.data
 $data | Set-Content "../friday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[9].response.stream.data
+$data = Parse-GzipStreamData $CombinedData.run.executions[8].response.stream.data
 $data | Set-Content "../saturday.json"
+$data = Parse-GzipStreamData $CombinedData.run.executions[9].response.stream.data
+$data | Set-Content "../sunday.json"
 $data = Parse-GzipStreamData $CombinedData.run.executions[10].response.stream.data #callout 11 is full per game
 $data | Set-Content "../fullpergame.json"
 $data = Parse-GzipStreamData $CombinedData.run.executions[11].response.stream.data #today's scoreboard
 $data | Set-Content "../scoreboard.json"
 $data = Parse-GzipStreamData $CombinedData.run.executions[12].response.stream.data #this week's scoreboard
-$data | Set-Content "../scoreboardSunday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[13].response.stream.data
 $data | Set-Content "../scoreboardMonday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[14].response.stream.data
+$data = Parse-GzipStreamData $CombinedData.run.executions[13].response.stream.data
 $data | Set-Content "../scoreboardTuesday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[15].response.stream.data
+$data = Parse-GzipStreamData $CombinedData.run.executions[14].response.stream.data
 $data | Set-Content "../scoreboardWednesday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[16].response.stream.data
+$data = Parse-GzipStreamData $CombinedData.run.executions[15].response.stream.data
 $data | Set-Content "../scoreboardThursday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[17].response.stream.data
+$data = Parse-GzipStreamData $CombinedData.run.executions[16].response.stream.data
 $data | Set-Content "../scoreboardFriday.json"
-$data = Parse-GzipStreamData $CombinedData.run.executions[18].response.stream.data
+$data = Parse-GzipStreamData $CombinedData.run.executions[17].response.stream.data
 $data | Set-Content "../scoreboardSaturday.json"
+$data = Parse-GzipStreamData $CombinedData.run.executions[18].response.stream.data
+$data | Set-Content "../scoreboardSunday.json"
 $data = Parse-GzipStreamData $CombinedData.run.executions[19].response.stream.data #callout 20 is allplayers
 $data | Set-Content "../allplayers.json"
 
